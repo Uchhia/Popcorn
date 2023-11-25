121175d2253573c6c51d8e877744421e492d2827
Rahul Pachauri <rpachauri584@gmail.com>
2023-11-25 17:54:01 +0530

    modification

--- usepopcorn/src/AppV2.js
+++ usepopcorn/src/AppV2.js
@@ -405,9 +405,15 @@ function WatchMovie({ movie, handleWatchedDelete }) {
 }
 
 function Summary({ watched }) {
-  const avgImdbRating = average(watched.map((movie) => movie.imdbRating));
-  const avgUserRating = average(watched.map((movie) => movie.userRating));
-  const avgRuntime = average(watched.map((movie) => movie.runtime));
+  const avgImdbRating = average(
+    watched ? watched.map((movie) => movie.imdbRating) : []
+  );
+  const avgUserRating = average(
+    watched ? watched.map((movie) => movie.userRating) : []
+  );
+  const avgRuntime = average(
+    watched ? watched.map((movie) => movie.runtime) : []
+  );
   return (
     <div className="summary">
       <h2>Movies you watched</h2>
